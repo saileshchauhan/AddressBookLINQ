f1de6b8625093838cdddb7defe5af4800f06bec2
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-29 09:30:52 +0530

    [sailesh] Add: Create DataTabe AdressBook

---
+++ AddressBookLINQ/AdBookDataTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLINQ
+{
+    class AdBookDataTable
+    {
+        DataTable adressBookTable = new DataTable();
+        
+    }
+}
---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
